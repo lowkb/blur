da7cc45352d7b96c2cf126f6af13e791deee1860
low_kb <69186337+lowkb@users.noreply.github.com>
2025-12-23 20:14:32 +0100

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -1,19 +1,4 @@
+import { render } from "preact";
+import { App } from "./src/app";
 
-const links = [
-  { href: 'https://github.com/lowkb', label: 'GitHub' },
-  { href: 'https://discord.com/users/123456789', label: 'Discord' },
-  { href: 'https://instagram.com/lowkb', label: 'Instagram' }
-];
-
-
-import { h, render } from 'preact';
-import Layout from './src/layouts/Layout.jsx';
-import Profile from './src/components/Profile.jsx';
-import './src/assets/styles/style.css';
-
-render(
-  <Layout>
-    <Profile />
-  </Layout>,
-  document.getElementById('root')
-);
+render(<App />, document.getElementById("app")!);
